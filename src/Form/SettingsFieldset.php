95036768ee7a810f83904b405a5951437b0f8c88
Daniel Berthereau <Daniel.github@Berthereau.net>
2020-10-19 00:00:00 +0000

    Updated strings.

--- src/Form/SettingsFieldset.php
+++ src/Form/SettingsFieldset.php
@@ -6,7 +6,7 @@ use Zend\Form\Fieldset;
 
 class SettingsFieldset extends Fieldset
 {
-    protected $label = 'Analytics Snippet Piwik / Matomo'; // @translate
+    protected $label = 'Analytics Snippet Matomo (Piwik)'; // @translate
 
     public function init()
     {
@@ -15,17 +15,17 @@ class SettingsFieldset extends Fieldset
                 'name' => 'analyticssnippetpiwik_tracker_url',
                 'type' => Element\Text::class,
                 'options' => [
-                    'label' => 'Piwik tracker api url', // @translate
+                    'label' => 'Matomo tracker api url', // @translate
                 ],
                 'attributes' => [
-                    'placeholder' => 'https://stats.example.com/piwik.php',
+                    'placeholder' => 'https://stats.example.com/matomo.php',
                 ],
             ])
             ->add([
                 'name' => 'analyticssnippetpiwik_site_id',
                 'type' => Element\Text::class,
                 'options' => [
-                    'label' => 'Piwik site id', // @translate
+                    'label' => 'Matomo site id', // @translate
                 ],
                 'attributes' => [
                     'placeholder' => '1',
@@ -35,8 +35,8 @@ class SettingsFieldset extends Fieldset
                 'name' => 'analyticssnippetpiwik_token_auth',
                 'type' => Element\Text::class,
                 'options' => [
-                    'label' => 'Piwik token authentication', // @translate
-                    'info' => 'API token with at least Admin permission in order to save visitor ip. See https://piwik.org/faq/general/faq_114/', // @translate
+                    'label' => 'Matomo token authentication', // @translate
+                    'info' => 'API token with at least Admin permission in order to save visitor ip. See https://matomo.org/faq/general/faq_114/', // @translate
                 ],
                 'attributes' => [
                     'placeholder' => '4fbb1496d7b563acbfe08a6f07a061c5',
