ec08665bea196b40137667d6e3ab8976cb0be290
Daniel Berthereau <Daniel.git@Berthereau.net>
2022-06-20 00:00:00 +0000

    Cleaned code.

--- src/Form/SettingsFieldset.php
+++ src/Form/SettingsFieldset.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 namespace AnalyticsSnippetPiwik\Form;
 
 use Laminas\Form\Element;
--- src/Tracker/Matomo.php
+++ src/Tracker/Matomo.php
@@ -1,4 +1,5 @@
 <?php declare(strict_types=1);
+
 namespace AnalyticsSnippetPiwik\Tracker;
 
 use AnalyticsSnippet\Tracker\AbstractTracker;
