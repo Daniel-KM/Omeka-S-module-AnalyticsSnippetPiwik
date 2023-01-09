07cb314652d861d7616d7181c15d2fee214edafa
Daniel Berthereau <Daniel.git@Berthereau.net>
2023-01-09 00:00:00 +0000

    Used Omeka S v4 feature "element group" for settings.

--- src/Form/SettingsFieldset.php
+++ src/Form/SettingsFieldset.php
@@ -9,14 +9,20 @@ class SettingsFieldset extends Fieldset
 {
     protected $label = 'Analytics Snippet Matomo (Piwik)'; // @translate
 
+    protected $elementGroups = [
+        'analytics' => 'Analytics', // @translate
+    ];
+
     public function init(): void
     {
         $this
             ->setAttribute('id', 'analytics-snippet-matomo')
+            ->setOption('element_groups', $this->elementGroups)
             ->add([
                 'name' => 'analyticssnippetpiwik_tracker_url',
                 'type' => Element\Text::class,
                 'options' => [
+                    'element_group' => 'analytics',
                     'label' => 'Matomo tracker api url', // @translate
                 ],
                 'attributes' => [
@@ -27,6 +33,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'analyticssnippetpiwik_site_id',
                 'type' => Element\Text::class,
                 'options' => [
+                    'element_group' => 'analytics',
                     'label' => 'Matomo site id', // @translate
                 ],
                 'attributes' => [
@@ -37,6 +44,7 @@ class SettingsFieldset extends Fieldset
                 'name' => 'analyticssnippetpiwik_token_auth',
                 'type' => Element\Text::class,
                 'options' => [
+                    'element_group' => 'analytics',
                     'label' => 'Matomo token authentication', // @translate
                     'info' => 'API token with at least Admin permission in order to save visitor ip. See https://matomo.org/faq/general/faq_114/', // @translate
                 ],
